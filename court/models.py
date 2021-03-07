c1896a72f0369812a225bbc3827e860f45e80d04
Shivam Balwani <201851122@iiitvadodara.ac.in>
2021-03-07 18:44:55 +0530

    Registration and login added

--- court/models.py
+++ court/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+CHOICES = [
+    ("Judge", "Judge"),
+    ("Lawyer", "Lawyer"),
+]
+
+
+class UserProfile(models.Model):
+    # required by the auth model
+    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=30, choices=CHOICES)
+    court = models.CharField(max_length=100, blank=False)
+    district = models.CharField(max_length=100, blank=False)
+    license_no = models.CharField(max_length=17, null=False)
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    """Create a matching profile whenever a user object is created."""
+    if created:
+        profile, new = UserProfile.objects.get_or_create(user=instance)
+
 
 # Create your models here.
--- court/views.py
+++ court/views.py
@@ -1,3 +1,90 @@
 from django.shortcuts import render
+from django.views.generic import *
+from django.views.generic.edit import *
+from .models import *
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import *
+from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-# Create your views here.
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = "court/registration_form.html"
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            password1 = form.cleaned_data["password1"]
+            user_type = form.cleaned_data["user_type"]
+            court = form.cleaned_data["court"]
+            district = form.cleaned_data["district"]
+            license_no = form.cleaned_data["license_no"]
+            if password == password1:
+                user.set_password(password)
+                user.save()
+                user = user.objects.get(username=username)
+                user_profile = UserProfile.objects.get(user=user)
+                user_profile.user_type = user_type
+                user_profile.court = court
+                user_profile.district = district
+                user_profile.license_no = license_no
+                user_profile.save()
+                messages.success(request, "Account register successfully")
+                return redirect("court:login")
+            else:
+                messages.success(request, "Password does not match")
+        return render(request, self.template_name, {"form": form})
+
+
+class LoginView(View):
+    form_class = LoginForm
+    template_name = "court/login.html"
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                # messages.info(request, 'Your have successfully loged in!')
+                return redirect("music:index")
+            else:
+                return render(
+                    request,
+                    "court/login.html",
+                    {"error_message": "Your account has been disabled"},
+                )
+        else:
+            return render(
+                request,
+                "music/login.html",
+                {"form": form, "error_message": "Invalid login"},
+            )
+
+
+class LogoutView(View):
+    form_class = LoginForm
+    template_name = "music/login.html"
+
+    def get(self, request):
+        form = self.form_class(None)
+        logout(request)
+        return redirect(reverse("music:login_user"))
