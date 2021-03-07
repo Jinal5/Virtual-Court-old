c31c69725e65ecdf08fbe095f26eb5e056381fa9
Raghvi Saxena <54863450+raghvis@users.noreply.github.com>
2021-03-07 13:33:02 +0530

    Update views.py

--- court/views.py
+++ court/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+def home(request):
+    return render(request, 'court/home.html', {'title' : 'Home'} )
+
+def about(request):
+    return render(request, 'court/about.html', {'title' : 'About'}) 
+
 # Create your views here.
