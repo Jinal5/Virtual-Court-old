9c6c96616ebcd5e053cf01804cd39dbcca0de832
Raghvi Saxena <201951119@iiitvadodara.ac.in>
2021-03-09 16:17:23 +0530

    postgre

--- ecourt/settings.py
+++ ecourt/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = "ecourt.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "ecourt",
+        "USER": "postgres",
+    "PASSWORD": "Manu#123@",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
