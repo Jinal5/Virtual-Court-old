852ee5212d48b79abc2fadb58ae5a77df1580392
Jinal Patel <201951197@iiitvadodara.ac.in>
2021-04-11 15:55:00 +0530

    Update admin.py

--- court/admin.py
+++ court/admin.py
@@ -4,8 +4,5 @@ from .models import *
 admin.site.register(UserProfile)
 admin.site.register(Advocate)
 admin.site.register(Case)
-<<<<<<< HEAD
-=======
 admin.site.register(Judge)
->>>>>>> 80025140d39514c1cc78f16240f96bad1ab73e05
 # Register your models here.
